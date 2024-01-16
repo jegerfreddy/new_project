8025f8e63b1a9b82c2fa66df84cc8e608a61433c
Vglomsaas <vetle.k@hotmail.no>
2024-01-16 17:01:21 +0100

    changed data stored in qr code to string since i get version error when trying to store bytes to qr code

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import qrcode
 from PIL import Image
 
 # Here we specify the data that we are going to store inside the QR code image.
-image_path = "./data/img.png"
+#TODO: Can't save data img to qr code
+image_path = "./data/img.jpg"
 image = Image.open(image_path)
 
 image_bytes = image.tobytes()
@@ -14,15 +15,14 @@ image_bytes = image.tobytes()
 # Here we customize the properties of the QR code
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4
 )
 
-# Add the data to the customized QR code.
-qr.add_data(image_bytes)
-
+qr.add_data("Hello World!")
 qr.make(fit=True)
-img = qr.make_image(back_color=(255, 0, 0), fill_color=(255, 255, 255))
+
+img = qr.make_image(fill_color="red", back_color="black")
 
 img.save('images/qrcode.png')
